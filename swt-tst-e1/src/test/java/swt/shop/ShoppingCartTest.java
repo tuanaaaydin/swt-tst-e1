e698ccfe6e7734e497b86a6e5eeaba8a2c74c0c2
tuanaaaydin <tuana.aydin@icloud.com>
2026-07-01 21:59:13 +0200

    [RED] Tests: addItem, getItemCount, getTotal

--- swt-tst-e1/src/test/java/swt/shop/ShoppingCartTest.java
+++ swt-tst-e1/src/test/java/swt/shop/ShoppingCartTest.java
@@ -17,4 +17,24 @@ class ShoppingCartTest {
     void testEmptyCart_totalIsZero() {
         assertEquals(0.0, cart.getTotal());
     }
+
+    @Test
+    void testAddItem_singleItem() {
+        cart.addItem("Apfel", 0.99, 1);
+        assertEquals(1, cart.getItemCount());
+    }
+
+    @Test
+    void testGetTotal_singleItem() {
+        cart.addItem("Brot", 2.50, 2);
+        assertEquals(5.00, cart.getTotal(), 0.001);
+    }
+
+    @Test
+    void testGetTotal_multipleItems() {
+        cart.addItem("Milch", 1.20, 3);
+        cart.addItem("Käse", 3.50, 1);
+        assertEquals(7.10, cart.getTotal(), 0.001);
+    }
+
 }

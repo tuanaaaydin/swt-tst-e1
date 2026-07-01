21196153a59adbf0d439de88e91a4e3a533490b4
tuanaaaydin <tuana.aydin@icloud.com>
2026-07-01 21:59:31 +0200

    [GREEN] Impl: addItem(), getItemCount(), getTotal() mit Stream

--- swt-tst-e1/src/main/java/swt/shop/ShoppingCart.java
+++ swt-tst-e1/src/main/java/swt/shop/ShoppingCart.java
@@ -7,8 +7,18 @@ public class ShoppingCart {
 
     private final List<CartItem> items = new ArrayList<>();
 
+    public void addItem(String name, double price, int quantity) {
+        items.add(new CartItem(name, price, quantity));
+    }
+
     public double getTotal() {
-        return 0.0;
+        return items.stream()
+                    .mapToDouble(item -> item.price() * item.quantity())
+                    .sum();
+    }
+
+    public int getItemCount() {
+        return items.size();
     }
 
     public record CartItem(String name, double price, int quantity) {}
